74dfd4a79d072efb4cdb4bc7f2d3102e430da3ac
LuisilloP <luisaraya98@hotmail.com>
2024-09-26 02:37:20 -0300

    update: database and images

--- src/types/Product.ts
+++ src/types/Product.ts
@@ -1,8 +1,12 @@
+
+type typeProduct = 'ramo' | 'otro' | 'box'
 export interface Product {
+    [x: string]: any;
     id: string;
     nameProduct: string;
     extra?: string;
     price: number;
     images: string[];
     description: string;
+    type: typeProduct
 }
